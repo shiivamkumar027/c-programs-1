92d746539e816a97c2c9d981b11143145b4e7174
shivamthakur777 <163742486+shivamthakur777@users.noreply.github.com>
2024-09-26 20:34:50 +0530

    Add files via upload

---
+++ even.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main(){
+    int a;
+    printf("Enter a number");
+    scanf("%d",&a);
+    if(a%2==0)
+    {
+    printf("Even no");
+    }
+    else
+    {
+    printf("Not a even No");
+}
+return 0;
+}
