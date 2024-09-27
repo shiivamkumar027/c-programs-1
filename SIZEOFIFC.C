e30b3ee3491b998acfd66c9d17ddc073d7afa35f
shivamthakur777 <163742486+shivamthakur777@users.noreply.github.com>
2024-09-27 12:02:00 +0530

    Add files via upload

---
+++ SIZEOFIFC.C
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main() {
+    int intType;
+    float floatType;
+    char charType;
+    printf("Size of int: %zu bytes\n", sizeof(intType));
+    printf("Size of float:=%zu bytes\n",sizeof(floatType));
+    printf("Size of char:=%zu bytes\n",sizeof(charType));
+	return 0;
+}
+
