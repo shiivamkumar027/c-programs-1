367641cf16e7cbce1cb5296ed0abb8203e48933e
shivamthakur777 <163742486+shivamthakur777@users.noreply.github.com>
2024-09-27 12:36:41 +0530

    Add files via upload

---
+++ evenorodd.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+int main()
+{
+int a;
+	printf("Enter a number=\n");
+	scanf("%d",&a);
+	if(a%2==0)
+	{
+		printf("Even No");
+	}
+	else
+	{
+		printf("Odd no");
+	}
+return 0;
+}
