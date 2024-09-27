595db30c95c64c7cdb8be08a70bcadbf2dffb601
shivamthakur777 <163742486+shivamthakur777@users.noreply.github.com>
2024-09-27 12:36:57 +0530

    Add files via upload

---
+++ Largestbetweenthree.c
@@ -0,0 +1,24 @@
+#include<stdio.h>
+int main()
+{
+	int a=30;
+	int b=40;
+	int c=23;
+	if (a>b && a<c)
+{
+
+printf("A is greator");
+}
+else if(b>a && b>c)
+{
+	printf("B is greatror");
+}
+else if(c>a && c>b)
+{
+	printf("C is greator");
+}
+else if(a==b==c){
+	printf("No are equyal");
+}
+return 0;
+}
