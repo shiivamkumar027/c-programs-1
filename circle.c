3e7d3af8fe05166e4c2d3430d5bfaa462251d179
shivamthakur777 <163742486+shivamthakur777@users.noreply.github.com>
2024-12-07 09:47:18 +0530

    Add files via upload

---
+++ circle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+void main()
+{
+    int r=4;
+    float pi=3.14;
+    float area=3.14*r*r;
+    printf("Area of circle is=%f\n", area);
+   float perimetre=2*3.14*r;
+    printf("Perimetre is=%f\n", perimetre);
+    return 0;
+
+}
